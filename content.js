398862ab2cb4c9c66bcf2584c03f0a54472b86a1
Peter Keogan <peter@pkeogan.com>
2025-04-22 13:27:52 -0700

    wip

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-if (window.location.hostname.endsWith('.npr.mykronos.com')) {
-    let subdomain = window.location.hostname.slice(0, -'.npr.mykronos.com'.length);
-    if (subdomain.includes('-uat-tsc')) {
+if (window.location.hostname.endsWith('.mykronos.com')) {
+    let subdomain = window.location.hostname.slice(0, -'.mykronos.com'.length);
+    if (subdomain.includes('uat-tsc')) {
         let style = document.createElement('style');
         style.textContent = '.navbar-header, .navbar-brand, .header, .mainNav, .navLink { background-color: #9f0712 !important; color: #fef2f2 !important; }';
         document.head.appendChild(style);
