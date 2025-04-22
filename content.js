cd3667fdc54e158ef83107f8f16246f9822a6501
Peter Keogan <peter@pkeogan.com>
2025-04-22 12:57:55 -0700

    v1

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ if (window.location.hostname.endsWith('.npr.mykronos.com')) {
     let subdomain = window.location.hostname.slice(0, -'.npr.mykronos.com'.length);
     if (subdomain.includes('-uat-tsc')) {
         let style = document.createElement('style');
-        style.textContent = '.header, .mainNav { background-color: #e7000b !important; }';
+        style.textContent = '.navbar-header, .navbar-brand, .header, .mainNav, .navLink { background-color: #9f0712 !important; color: #fef2f2 !important; }';
         document.head.appendChild(style);
     }
 }
